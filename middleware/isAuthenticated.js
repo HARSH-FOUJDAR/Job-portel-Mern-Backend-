0ff0c6e9f947e8875e889859748740143f07ae8e
Harsh Foujdar <harshbtp738@gmail.com>
2026-02-12 08:43:18 +0530

    xz

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,32 +1,25 @@
 import jwt from "jsonwebtoken";
 
-export const isAuthenticated = (req, res, next) => {
+export const isAuthenticated = async (req, res, next) => {
   try {
     const token = req.cookies.token;
-
     if (!token) {
-      return res.status(401).json({
-        message: "Unauthorized: Token not provided",
+      res.status(401).json({
+        message: "unauthorized",
         success: false,
       });
     }
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
     if (!decoded) {
-      return res.status(401).json({
+      res.status(401).json({
         message: "Invalid Token",
         success: false,
       });
     }
-
     req.id = decoded.userId;
     next();
   } catch (error) {
-    console.log("Error in isAuthenticated:", error);
-    return res.status(401).json({
-      message: "Invalid Token",
-      success: false,
-    });
+    console.log("error in isAuthenticated", error);
   }
 };
+
